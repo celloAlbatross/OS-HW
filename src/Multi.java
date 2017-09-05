2ec41ec98d9dd8f71d0580adbb66e004202e9e29
FABIANA <Fabiana@FABIANAs-MacBook-Pro.local>
2017-09-05 15:01:22 +0700

    Create array of Obj Theater

--- src/Multi.java
+++ src/Multi.java
@@ -1,18 +1,18 @@
 import java.util.Random;
 
 class Theater{
-    int[] theater = new int[5];
+    int thearter;
 }
 
 class TicketMachine extends Thread{
     Random rand = new Random();
     public String myName;
     public Thread thread;
-    public Theater c;
+    public Theater[] c;
     private static final int MAX_MOV = 5;
 
 
-    TicketMachine(String name, Theater c1){
+    TicketMachine(String name, Theater[] c1){
         myName = name;
         c = c1;
         System.out.println("Creating " + myName);
@@ -24,17 +24,17 @@ class TicketMachine extends Thread{
                 int theRestOfTick = 0;
                 boolean isLock = false;
                 for (int i=0; i<MAX_MOV; i++){
-                    theRestOfTick += c.theater[i];
+                    theRestOfTick += c[i].thearter;
                 }
                 int randMov = rand.nextInt(MAX_MOV);
                 int randTick = rand.nextInt(5) + 1;
                 int delay = (int)(Math.random()*500);
                 System.out.println("Before Lock ");
                 for(int i=0; i<MAX_MOV; i++) {
-                    System.out.println("Theater" + i + ": " + c.theater[i]);
+                    System.out.println("Theater" + i + ": " + c[i].thearter);
                 }
-                if(c.theater[randMov] - randTick >= 0) {
-                    c.theater[randMov] -= randTick;
+                if(c[randMov].thearter - randTick >= 0) {
+                    c[randMov].thearter -= randTick;
                     isLock = true;
                 }
                 Thread.sleep(delay);
@@ -42,15 +42,15 @@ class TicketMachine extends Thread{
 
                 if(isBuy == 1 && isLock) {
 //                    Thread.sleep(delay);
-                    System.out.println("Before " + myName +" Buy Theater: " + (randMov+1) + " = " + ((c.theater[randMov]) + randTick));
+                    System.out.println("Before " + myName +" Buy Theater: " + (randMov+1) + " = " + ((c[randMov].thearter) + randTick));
                     System.out.println(myName + " Buy theater: " + (randMov+1) + " for " + randTick);
                     for(int i=0; i<MAX_MOV; i++) {
-                        System.out.println("Theater " + (i+1) + ": " + c.theater[i]);
+                        System.out.println("Theater " + (i+1) + ": " + c[i].thearter);
                     }
                 }else {
-                    System.out.println(myName + " Can't Buy / Not Buy " );
+//                    System.out.println(myName + " Can't Buy / Not Buy " );
                     if(isLock)
-                        c.theater[randMov] += randTick;
+                        c[randMov].thearter += randTick;
                 }
             }
 
@@ -74,9 +74,17 @@ public class Multi {
     private static final int MAX_SEAT = 20;
     private static final int MAX_MOV = 5;
     public static void main(String args[]){
-        Theater c = new Theater();
+        Theater[] c = new Theater[MAX_MOV];
+
+        for (int i=0; i<MAX_MOV; i++) {
+            c[i] = new Theater();
+        }
+
+        System.out.println("debug");
+        System.out.println(c[0].thearter);
+
         for(int i=0; i<MAX_MOV; i++) {
-            c.theater[i] = MAX_SEAT;
+            c[i].thearter = MAX_SEAT;
         }
         TicketMachine t1 = new TicketMachine("t1", c);
         TicketMachine t2 = new TicketMachine("t2", c);
